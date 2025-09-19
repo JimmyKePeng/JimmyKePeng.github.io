8c4d9a337e2b9c2bb307aa913fcb2c2ca9d02744
KegangPeng <Kegangpeng1@gmail.com>
2025-09-18 19:52:43 -0700

    testing

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", function handleClick() {
-  const w = parseFloat(document.getElementById("weight").value);
-  const h = parseFloat(document.getElementById("height").value);
-  const result = w / ((h * h) / 10000);
-  const BMI = document.getElementById("BMI");
-  BMI.value = result.toFixed(2);
-});
